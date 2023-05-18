fe85f8ff21a69e77c1bb6e8b0e8e5b16b0593a60
Blro <gyeongtae020319@gmail.com>
2023-05-18 16:20:58 +0900

    docs: Get Started, API Reference

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,28 +1,41 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Body Scroll Lock",
-  description: "Modern body scroll locking library",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+  locales: {
+    root: {
+      label: "English",
+      lang: "en-US",
+      description: "Modern body scroll locking library",
+      themeConfig: {
+        nav: [
+          { text: "Get Started", link: "/get-started/" },
+          { text: "API", link: "/api/" },
+        ],
+        socialLinks: [
+          {
+            icon: "github",
+            link: "https://github.com/blro0319/body-scroll-lock",
+          },
+        ],
+      },
+    },
+    ko: {
+      label: "한국어",
+      lang: "ko-KR",
+      description: "전역 스크롤를 막는 현대적인 라이브러리",
+      themeConfig: {
+        nav: [
+          { text: "시작하기", link: "/ko/get-started/" },
+          { text: "API", link: "/ko/api/" },
+        ],
+        socialLinks: [
+          {
+            icon: "github",
+            link: "https://github.com/blro0319/body-scroll-lock",
+          },
+        ],
+      },
+    },
+  },
+});
---
+++ docs/ko/index.md
@@ -0,0 +1,26 @@
+---
+layout: home
+
+hero:
+  name: Body Scroll Lock
+  text: Modern body scroll locking library
+  tagline: TypeScript, 의존성 없음, 작은 용량
+  actions:
+    - theme: brand
+      text: 시작하기
+      link: /ko/get-started/
+    - theme: alt
+      text: API 레퍼런스
+      link: /ko/api/
+
+features:
+  - title: iOS 동작
+    icon: 🍎
+    details: 실제 iOS 기기에서 테스트되었습니다.
+  - title: TypeScript
+    icon: 🛠
+    details: TypeScript로 작성되었습니다.
+  - title: 작은 용량
+    icon: 📦
+    details: 의존성 없이 1KB 미만으로 압축되었습니다.
+---
