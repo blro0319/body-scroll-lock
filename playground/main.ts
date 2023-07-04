2fadef1ff83428e1c977397d809df00ced04d57a
Blro <gyeongtae020319@gmail.com>
2023-07-04 17:16:37 +0900

    chore: Add reset test in playground

--- playground/main.ts
+++ playground/main.ts
@@ -1,11 +1,29 @@
-import { disableBodyScroll, enableBodyScroll } from "../src";
+import { disableBodyScroll, enableBodyScroll, resetBodyScroll } from "../src";
+
+const out = document.getElementById("out");
+function updateOut() {
+  if (!out) return;
+
+  const count = window.__bodyScrollLock?.count ?? 0;
+  if (count === 0) out.innerText = "Unlocked";
+  else if (count === 1) out.innerText = "Locked 1 time";
+  else out.innerText = `Locked ${count} times`;
+}
 
 const disableButton = document.getElementById("disable");
 disableButton?.addEventListener("click", () => {
   disableBodyScroll();
+  updateOut();
 });
 
 const enableButton = document.getElementById("enable");
 enableButton?.addEventListener("click", () => {
   enableBodyScroll();
+  updateOut();
+});
+
+const resetButton = document.getElementById("reset");
+resetButton?.addEventListener("click", () => {
+  resetBodyScroll();
+  updateOut();
 });
