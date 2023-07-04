aef1983e786128dbd7132edafaaeb29b83d6b346
Blro <gyeongtae020319@gmail.com>
2023-07-04 14:02:57 +0900

    build: Change build file name

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,12 +1,22 @@
-import { resolve } from "path";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    outDir: "./dist",
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "@blro/body-scroll-lock",
-      fileName: "body-scroll-lock",
+      fileName(format) {
+        if (format === "es") return "body-scroll-lock.mjs";
+        return "body-scroll-lock.cjs";
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "src/"),
+      "#playground": resolve(__dirname, "playground/"),
     },
   },
 });
